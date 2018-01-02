6e51ae2adf17c3957dbe53975b87e076ab19b58c
jaimergp <jaime.rogue@gmail.com>
2018-01-02 12:02:00 +0100

    Add classifiers and entry_points to setup.py

--- setup.py
+++ setup.py
@@ -12,8 +12,20 @@ setup(
   url = 'https://github.com/bobbypaton/goodvibes',
   download_url = 'https://github.com/bobbypaton/GoodVibes/archive/2.0.1.zip',
   keywords = ['compchem', 'thermochemistry', 'gaussian', 'vibrational-entropies', 'temperature'],
-  classifiers = [],
+  classifiers=[
+    'Programming Language :: Python',
+    'Natural Language :: English',
+    'Environment :: Console',
+    'Intended Audience :: Science/Research',
+    'License:: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Topic :: Scientific/Engineering :: Chemistry',
+  ],
   install_requires=["numpy", ],
   python_requires='>=2.6',
   include_package_data=True,
+  entry_points='''
+    [console_scripts]
+    goodvibes=goodvibes.GoodVibes:main
+    '''
 )
